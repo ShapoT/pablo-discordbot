cbeef1f5aaa59b254b77c684272e1b73c1a67148
ShapoT <git@maxenceplessis.fr>
2022-09-05 19:18:43 +0200

    Added a command

---
+++ src/commands/utils/review.js
@@ -0,0 +1,35 @@
+const {
+    SlashCommandBuilder,
+    ModalBuilder,
+    TextInputBuilder,
+    TextInputStyle,
+    ActionRowBuilder,
+  } = require("discord.js");
+  
+  module.exports = {
+    data: new SlashCommandBuilder()
+      .setName("review")
+      .setDescription("Pour soumettre votre projet à l'analyse du Discord."),
+  
+    async execute(interaction, client) {
+      const modal = new ModalBuilder()
+        .setCustomId("review-modal")
+        .setTitle("Code review");
+  
+      const linkInput = new TextInputBuilder()
+        .setCustomId("linkInput")
+        .setLabel("Entrez le lien du repository GitHub de votre projet")
+        .setPlaceholder("https://github.com/Johndoe/MyProject")
+        .setStyle(TextInputStyle.Short);
+    
+      const firstActionRowBuilder = new ActionRowBuilder().addComponents(
+        linkInput
+      );
+  
+      modal.addComponents(
+        firstActionRowBuilder,
+      );
+  
+      await interaction.showModal(modal);
+    },
+  };  
---
+++ src/components/modals/review-modal.js
@@ -0,0 +1,30 @@
+const { EmbedBuilder, messageLink } = require("discord.js");
+
+module.exports = {
+  data: {
+    name: "review-modal",
+    description: "Pour soumettre votre projet à l'analyse du Discord.",
+  },
+
+  async execute(interaction, client) {
+    await interaction.reply({
+      content: "Votre message a été envoyé !",
+      ephemeral: true,
+    });
+    const reviewEmbed = new EmbedBuilder()
+      .setColor(0x2ecc71)
+      .addFields({
+        name: "Code review",
+        value: "**" + "<@"+interaction.user.id +">"+ "** souhaiterais un avis sur son projet :",
+      })
+      .addFields({
+        name: "Lien du repository",
+        value: interaction.fields.getTextInputValue("linkInput"),
+      })
+      .setTimestamp()
+      .setFooter({ text: "Log system of PabloCoding" });
+    client.channels.cache
+      .get("970113913481986158")
+      .send({ embeds: [reviewEmbed] });
+  },
+};
