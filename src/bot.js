07382d32d0f7e9a98a4204acb9f967fa8fa05c65
Space V <40030799+TheSpaceGamerV2@users.noreply.github.com>
2022-09-06 17:51:31 +0200

    Added intents

--- src/bot.js
+++ src/bot.js
@@ -4,7 +4,14 @@ const { token } = process.env;
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const fs = require("fs");
 
-const client = new Client({ intents: 32767 });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ]
+});
 client.commands = new Collection();
 client.buttons = new Collection();
 client.selectMenus = new Collection();
